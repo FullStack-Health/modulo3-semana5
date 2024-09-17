d68cc5dbea5dccbaed9758eb7e8a26cff84f692f
Andre <andresnu@tutanota.com>
2024-09-17 19:23:52 -0300

    feat: aula2

--- demo/src/test/java/com/example/demo/TaskServiceUnitTest.java
+++ demo/src/test/java/com/example/demo/TaskServiceUnitTest.java
@@ -8,6 +8,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class) // Adiciona o Mockito
@@ -22,13 +24,36 @@ class TaskServiceUnitTest {
     @Test
     void adicionaTask() {
 
+        // Dado
         String teste = "teste";
         Task task = new Task(teste);
         when(repository.save(any())).thenReturn(task);
 
+        // Quando
         Task resultado = taskService.adicionaTask(teste);
 
+        // Então
         assertEquals(teste, resultado.getTask());
+    }
+
+    @Test
+    void adicionaTaskBDD() {
+
+        // Dado
+        String teste = "teste";
+        Task task = new Task(teste);
+        given(repository.save(any())).willReturn(task);
+
+        // Quando
+        Task resultado = taskService.adicionaTask(teste);
+
+        // Então
+        assertEquals(teste, resultado.getTask());
+        then(repository)
+                .should()
+                .save(any());
 
     }
+
+
 }
